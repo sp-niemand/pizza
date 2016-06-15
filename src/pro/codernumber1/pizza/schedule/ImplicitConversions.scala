36cef8e715a73f8078cbd1a8a1366dce6786456b
Dmitri Cherepovski <codernumber1@gmail.com>
2016-06-15 03:44:01 +0300

    fix average for empty schedule

--- src/pro/codernumber1/pizza/schedule/ImplicitConversions.scala
+++ src/pro/codernumber1/pizza/schedule/ImplicitConversions.scala
@@ -2,7 +2,7 @@ package pro.codernumber1.pizza.schedule
 
 object ImplicitConversions {
   implicit class Schedule(s: Seq[ScheduledJob]) {
-    def averageFinishTime: Long = s.view.map(_.waitTime.toLong).sum / s.length
+    def averageFinishTime: Long = if (s.nonEmpty) s.view.map(_.waitTime.toLong).sum / s.length else 0
     def finish: Int = s.lastOption.map(_.finishTime).getOrElse(0)
   }
 }
